98906268b7673cc87d011c568c96f08cc11d5b0b
Your Name <you@example.com>
2022-02-05 00:35:19 +0530

    Added: Initial packages & Initial models, schemas

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+
+const zomato = express();
+
+// MiddleWares
+zomato.use(express.json());
+zomato.use(express.urlencoded({extended:false}));
+zomato.use(helmet());
+zomato.use(cors());
+
+zomato.get("/",(req, res)=>res.json({message:"Setup done"}));
+
+zomato.listen(3001,()=>console.log("Server is running"))
