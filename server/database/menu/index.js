04f7e6430e349dd22299e2898d641a506bec25fe
Your Name <you@example.com>
2022-02-06 21:07:23 +0530

    Added: Remaining Models

---
+++ server/database/menu/index.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const MenuSchema = new mongoose.Schema({
+    menus: [{
+        name: { type: String, required: true },
+        items: [{
+            type: mongoose.Types.ObjectId,
+            ref: "Foods"
+        }]
+    }],
+    recommended: [{
+        type: mongoose.Types.ObjectId,
+        ref: "Foods",
+        unique:true
+    }]
+})
+
+export const MenuModel=mongoose.model("Menu",MenuSchema);
