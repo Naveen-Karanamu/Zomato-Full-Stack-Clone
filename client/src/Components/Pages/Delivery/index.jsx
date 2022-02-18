992af628ac07363753013aa544070f1a5ad43b7c
Your Name <you@example.com>
2022-02-18 18:08:19 +0530

    Updated: Restaurant iamge tabs to dynamic

--- client/src/Components/Pages/Delivery/index.jsx
+++ client/src/Components/Pages/Delivery/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 // components
 import FoodsDeliveryCarausal from '../../Carousel/Food.delivery.carousel';
@@ -7,6 +7,22 @@ import RestaurantCard from '../../Restaurants/Delivery.restaurants';
 
 
 export const Delivery = () => {
+
+  const [restaurantList, serRastaurantList] = useState([
+    {
+      _id:"1234",
+      photos:[
+        "https://b.zmtcdn.com/data/pictures/6/2800126/11b1ca01d1e39d2d619acebdfb70352e_o2_featured_v2.jpg",
+      ],
+      name:"Ramchandra Restaurant",
+      cuisine:["Biriyani", "Chinese", "North Indian", "Seafood", "Andhra"],
+      averageCost:350,
+      isPro:true,
+      isOff:15,
+      deliveryTime:47,
+      rating:4.4
+    }
+  ])
 
   return (
     <>
@@ -17,15 +33,11 @@ export const Delivery = () => {
           <h1 className='text-xl lg:text-3xl lg:font-semibold text-gray-700  md:text-3xl pb-2 md:pb-8 px-4'>Gajuwaja Restaurants, Vizag</h1>
         </div>
         <div className='md:px-4 md:flex flex-wrap justify-around lg:container lg:mx-auto lg:px-48 '>
-          <RestaurantCard />
-          <RestaurantCard />
-          <RestaurantCard />
-          <RestaurantCard />
-          <RestaurantCard />
-          <RestaurantCard />
-          <RestaurantCard />
-          <RestaurantCard />
-          <RestaurantCard />
+          {
+            restaurantList.map((list) => (
+              <RestaurantCard {...list} key={restaurantList._id}/>
+            ))
+          }
         </div>
       </div>
     </>
--- client/src/Components/Restaurants/Delivery.restaurants.jsx
+++ client/src/Components/Restaurants/Delivery.restaurants.jsx
@@ -3,31 +3,31 @@ import React from 'react'
 // Icons
 import { AiTwotoneStar } from "react-icons/ai"
 
-export const RestaurantCard = () => {
+export const RestaurantCard = (props) => {
     return (
         <>
-            <div className='px-4 pt-3 pb-3 w-full md:w-60 md:h-96 md:mb-10 lg:w-1/3 lg:h-full lg:px-6'>                
+            <div className='px-4 pt-3 pb-3 w-full md:w-60 md:h-96 md:mb-10 lg:w-1/3 lg:h-full lg:px-4'>
                 <div className='bg-white rounded-2xl shadow-md lg:hover:shadow-lg transtion duration-300 ease-in-out'>
                     <div className='relative'>
-                        <div className='w-full h-52 md:h-64'>
-                            <img src="https://b.zmtcdn.com/data/pictures/4/2800154/df4d54cda041929dd85d0ac00ee63233_o2_featured_v2.jpg" alt="biri" className='w-full h-full rounded-t-2xl md:rounded-2xl object-cover' />
+                        <div className='w-full h-52 md:h-64 lg:p-2'>
+                            <img src={props.photos.length && props.photos[0]} alt="biri" className='w-full h-full rounded-t-2xl md:rounded-2xl object-cover' />
                         </div>
-                        <div className='flex justify-between items-center relative '>
-                            <span className='text-white bg-zomatoRed-400 rounded-r text-sm px-2 absolute left-0 bottom-11'>Pro extra 15% OFF</span>
-                            <span className='text-white bg-blue-500 rounded-r text-sm px-2 absolute left-0 bottom-3'>10% OFF</span>
-                            <span className='bg-white p-1  rounded bg-opacity-80 text-sm absolute right-3 bottom-3'>12min</span>
+                        <div className='flex justify-between items-center relative'>
+                            {props.isPro && (<span className='text-white bg-zomatoRed-400 rounded-r text-sm px-2 absolute left-2 bottom-11 lg:bottom-14 '>Pro extra 15% OFF</span>)}
+                            {props.isOff && (<span className='text-white bg-blue-500 rounded-r text-sm px-2 absolute left-2 bottom-3 lg:bottom-6'>{props.isOff}% OFF</span>)}
+                            {props.deliveryTime && (<span className='bg-white p-1  rounded bg-opacity-80 text-sm absolute right-6 bottom-3 lg:bottom-6'>{props.deliveryTime} min</span>)}
                         </div>
                     </div>
                     <div className='p-3'>
                         <div className='flex justify-between items-center'>
-                            <h1 className='text-lg font-normal'>All Family Restaurant</h1>
-                            <div className='flex items-baseline text-white bg-green-700 rounded px-2 text-sm font-semibold gap-1'><p>4.9 </p><AiTwotoneStar className='w-3 h-3' /></div>
+                            <h1 className='text-lg font-normal'>{props.name}</h1>
+                            <div className='flex items-baseline text-white bg-green-700 rounded px-2 text-sm font-semibold gap-1'><p>{props.rating} </p><AiTwotoneStar className='w-3 h-3' /></div>
                         </div>
                         <div className='flex justify-between items-center text-xs text-gray-700'>
-                            <p>South Indian, Hydrabadi, Dum</p>
-                            <p>₹300 for one</p>
+                            <p>{props.cuisine.join(", ")}</p>
+                            <p>₹{props.averageCost} for one</p>
                         </div>
-                    </div>
+                    </div>   
                 </div>
             </div>
         </>
