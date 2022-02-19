b3852cd945ac966afd7c73368642e1ed33cd9bdf
Your Name <you@example.com>
2022-02-19 17:58:35 +0530

    Fixed: Grid's alignment

--- client/src/Components/Pages/Secondary/Restaurant/DefaultImageGrid.restaurant.jsx
+++ client/src/Components/Pages/Secondary/Restaurant/DefaultImageGrid.restaurant.jsx
@@ -18,30 +18,34 @@ const DefaultImageGrid = (props) => {
 
 
             <div className='hidden md:flex gap-2 lg:container lg:mx-auto lg:px-52 z-10'>
-                <div className='w-3/5 h-96 overflow-hidden'>
-                    <img src={props.images[0]}  className='w-full h-full object-cover transition duration-700 hover:scale-110 ' />
+                <div className='w-2/3 h-96 overflow-hidden'>
+                    <img src={props.images[0]} className='w-full h-full object-cover transition duration-700 hover:scale-110 ' />
                 </div>
-                <div className="w-2/5 flex h-96 gap-2">
-                    <div className="h-full flex flex-col gap-2 ">
+                <div className="w-1/3 flex h-96 gap-2">
+                    <div className="w-full h-full flex flex-col gap-2 ">
                         <div className="h-full overflow-hidden">
-                            <img src={props.images[1]}  className='w-full h-full object-cover transition duration-700 hover:scale-110 ' />
+                            <img src={props.images[1]} className='w-full h-full object-cover transition duration-700 hover:scale-110 ' />
                         </div>
                         <div className="h-full overflow-hidden">
-                            <img src={props.images[2]}  className='w-full h-full object-cover transition duration-700 hover:scale-110 ' />
+                            <img src={props.images[2]} className='w-full h-full object-cover transition duration-700 hover:scale-110 ' />
                         </div>
                     </div>
-                    <div className="h-full flex flex-col gap-2">
+                    <div className="w-full h-full flex flex-col gap-2">
                         <div className="h-full relative">
-                            <img src={props.images[3]}  className='w-full h-full object-cover ' />
+                            <div className="h-full">
+                                <img src={props.images[3]} className='w-full h-full object-cover ' />
+                            </div>
                             <div className="bg-black w-full h-full z-10 absolute top-0 left-0 opacity-50" />
                             <p className="text-white text-lg text-center z-20 w-full h-full absolute right-0 top-20 ">View Gallery</p>
                         </div>
                         <div className="h-full relative">
-                            <img src={props.images[4]}  className='w-full h-full object-cover' />
+                            <div >
+                                <img src={props.images[4]} className='w-full h-full object-cover' />
+                            </div>
                             <div className="bg-picGray-100 w-full h-full z-10 absolute top-0 left-0 opacity-90" />
-                            <div className="flex items-center text-white  flex-col justify-center absolute right-16 top-16 z-10">
+                            <div className="flex items-center text-white  flex-col justify-center absolute right-12 top-16 z-10">
                                 <div className="bg-gray-500 p-2 rounded-full">
-                                    <BsCamera className="w-6 h-6"/>
+                                    <BsCamera className="w-6 h-6" />
                                 </div>
                                 <p className="text-lg ">Add Photos</p>
                             </div>
