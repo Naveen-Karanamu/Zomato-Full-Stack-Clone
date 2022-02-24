d11eeef7f0594f20231e5baa57edf4e809d22423
Your Name <you@example.com>
2022-02-24 10:19:12 +0530

    Updated: Cart's Activation state

--- client/src/Components/Pages/Secondary/Restaurant/Order Online/Cart/CartContainer.jsx
+++ client/src/Components/Pages/Secondary/Restaurant/Order Online/Cart/CartContainer.jsx
@@ -1,19 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 // Icons
-import { IoMdArrowDropup, IoMdArrowDropright } from "react-icons/io"
+import { IoMdArrowDropup, IoMdArrowDropright, IoMdArrowDropdown } from "react-icons/io"
 import CartItems from './CartItems';
 import { IoClose } from "react-icons/io5"
 
-const CartContainerSm = () => {
+const CartContainerSm = ({ toggle, isOpen }) => {
   return (
     <>
       {/* <div className='fixed w-'>
         <CartItems />
       </div> */}
       <div className='fixed bottom-0 left-0 w-full z-20 bg-white p-4 border border-gray-300 shadow-2xl flex justify-between items-center'>
-        <div className='flex flex-col text-gray-600'>
-          <p className='flex itms-center gap-1'>1 item <IoMdArrowDropup className='w-5 h-5' /></p>
+        <div className='flex flex-col text-gray-600' onClick={toggle}>
+          <p className='flex itms-center gap-1'>1 item
+            {
+              isOpen ? <IoMdArrowDropup className='w-5 h-5' /> : <IoMdArrowDropdown className='w-5 h-5' />
+            }
+          </p>
           <p>₹250 (plus taxes)</p>
         </div>
         <div className='bg-zomatoRed-300 text-white py-2 px-14 rounded-md '>
@@ -24,13 +28,17 @@ const CartContainerSm = () => {
   )
 }
 
-const CartContainerMd = () => {
+const CartContainerMd = ({ toggle, isOpen }) => {
   return (
     <>
       <div className='fixed bottom-0 left-0 w-full z-20 bg-white p-4 border border-gray-300 shadow-2xl '>
         <div className='lg:container lg:mx-auto lg:px-52 flex justify-between items-center '>
           <div className='flex items-center gap-3 '>
-            <span className='border border-gray-300 rounded p-1'><IoMdArrowDropup className='w-5 h-5' /></span>
+            <span className='border border-gray-300 rounded p-1' onClick={toggle}>
+              {
+                isOpen ? <IoMdArrowDropup className='w-5 h-5' /> : <IoMdArrowDropdown className='w-5 h-5' />
+              }
+            </span>
             <p className='text-lg font-medium'> Your Order (2) </p>
           </div>
           <div className='flex items-center gap-4'>
@@ -44,31 +52,29 @@ const CartContainerMd = () => {
 }
 
 const CartContainer = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleCart = () => setIsOpen((prev) => !prev);
+
+  const closeCart = () => setIsOpen(false);
+
   return (
     <>
       <div className=''>
-        <div className='md:hidden'><CartContainerSm /></div>
-        <div className='hidden md:flex '><CartContainerMd /></div>
-        <div className='fixed w-full z-10 bg-white bottom-20 left-0 h-64 p-6'>
-          <div className='w-full  lg:container lg:mx-auto lg:px-52'>
-            <h2 className='text-xl text-gray-700 flex items-center justify-between'>Your Orders
-              <span className='text-black'><IoClose className='w-6 h-6' /></span>
-            </h2>
-            <hr className='mt-4' />
+        <div className='md:hidden'><CartContainerSm toggle={toggleCart} isOpen={isOpen} /></div>
+        <div className='hidden md:flex '><CartContainerMd toggle={toggleCart} isOpen={isOpen} /></div>
+        {
+          isOpen && <div className='fixed w-full z-10 bg-white bottom-20 left-0 h-64 p-6'>
+            <div className='w-full  lg:container lg:mx-auto lg:px-52'>
+              <h2 className='text-xl text-gray-700 flex items-center justify-between'>Your Orders
+                <span className='text-black'><IoClose onClick={closeCart} className='w-6 h-6' /></span>
+              </h2>
+              <hr className='mt-4' />
+            </div>
+            <div className='flex flex-col overflow-y-scroll  bg-white '>
+              <CartItems name="Chicken Dum Biriyani" price="250" quantity="1" />
+            </div>
           </div>
-          <div className='flex flex-col overflow-y-scroll  bg-white '>
-            <CartItems />
-            <CartItems />
-            <CartItems />
-            <CartItems />
-            <CartItems />
-            <CartItems />
-            <CartItems />
-            <CartItems />
-            <CartItems />
-            <CartItems />
-          </div>
-        </div>
+        }
       </div>
     </>
   )
--- client/src/Components/Pages/Secondary/Restaurant/Order Online/Cart/CartItems.jsx
+++ client/src/Components/Pages/Secondary/Restaurant/Order Online/Cart/CartItems.jsx
@@ -3,24 +3,24 @@ import React from 'react'
 // Icons
 import { BsFillTriangleFill } from "react-icons/bs"
 
-export const CartItems = () => {
+export const CartItems = (props) => {
     return (
         <>
             <div>
-                
+
                 <div className='w-full z-10  lg:container lg:mx-auto lg:px-52'>
                     <div className='flex justify-between items-start py-2'>
-                        <span className='flex items-center gap-3 text-sm md:text-lg'><BsFillTriangleFill className='border border-yellow-900 text-yellow-900 w-3 h-3 ' /> Chicken Dum Biriyani</span> 
+                        <span className='flex items-center gap-3 text-sm md:text-lg'><BsFillTriangleFill className='border border-yellow-900 text-yellow-900 w-3 h-3 ' /> {props.name}</span>
                         <div className='flex flex-col justify-end items-end'>
-                            <p className='text-gray-600 text-sm'>₹250</p>
-                            <div className='bg-zomatoRed-400 text-white px-3 rounded-lg gap-2 flex items-center text-sm py-1 '>
+                            <p className='text-gray-600 text-sm md:text-base'>₹{parseInt(props.price) * parseInt(props.quantity)}</p>
+                            <div className='bg-zomatoRed-400 text-white px-3 rounded-lg gap-2 flex items-center text-sm py-1 md:text-lg md:px-5'>
                                 <span>-</span>
                                 <span>10</span>
                                 <span>+</span>
                             </div>
                         </div>
                     </div>
-                    <hr className='mt-2'/>
+                    <hr className='mt-2' />
                 </div>
 
             </div>
