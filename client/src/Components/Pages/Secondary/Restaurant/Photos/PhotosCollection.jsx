f6677ea38666eda5fd2142d7e3f80988901192c3
Your Name <you@example.com>
2022-02-27 10:57:21 +0530

    Updated: Restaurant's Photos page

--- client/src/Components/Pages/Secondary/Restaurant/Photos/PhotosCollection.jsx
+++ client/src/Components/Pages/Secondary/Restaurant/Photos/PhotosCollection.jsx
@@ -7,7 +7,7 @@ export const PhotosCollection = (props) => {
         <>
             <div className="">               
                 <div onClick={props.openViewer} >
-                    <div className='w-32 h-32 rounded lg:rounded-lg md:w-48 md:h-48 lg:w-52 lg:h-52 overflow-hidden ' >
+                    <div className='w-28 h-28 rounded lg:rounded-lg md:w-36 md:h-36 lg:w-52 lg:h-52 overflow-hidden ' >
                         <img src={props.image} alt="menu" className='w-full h-full rounded-lg lg:hover:scale-110 transiton duration-700 object-cover' />
                     </div>
                     
